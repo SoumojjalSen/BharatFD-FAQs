654d77523760367ae64bc629f4a8e1867c1c14dc
SoumojjalSen <soumojjalsen@gmail.com>
2025-02-02 01:41:15 +0530

    feat: add Bootstrap styling to the navbar and page

---
+++ home/utils/languages.py
@@ -0,0 +1,5 @@
+LANGUAGES = {
+    "English": "en",
+    "Hindi": "hi",
+    "Bengali": "bn"
+}
--- home/views.py
+++ home/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render, redirect
-from .forms import FAQForm
-import requests
 from django.urls import reverse
+import requests
+from .forms import FAQForm
+from .utils.languages import LANGUAGES 
 
 def index(request):
-    if request.method == 'POST':
-        form = FAQForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = FAQForm()
-    
+    form = FAQForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('home')
+
     lang = request.GET.get('lang', 'en')
-    
+    api_url = request.build_absolute_uri(reverse('faq-list-create')) + f'?lang={lang}'
 
-    api_url = reverse('faq-list-create') + f'?lang={lang}'
-    response = requests.get(request.build_absolute_uri(api_url))
-
-    if response.status_code == 200:
-        faqs = response.json()
-    else:
+    try:
+        response = requests.get(api_url)
+        faqs = response.json() if response.status_code == 200 else []
+    except requests.RequestException:
         faqs = []
-            
+
     context = {
         'form': form,
         'faqs': faqs,
-        'lang': lang
+        'lang': lang,
+        'languages': LANGUAGES 
     }
-    return render(request, 'index.html', context)
 
+    return render(request, 'index.html', context)
