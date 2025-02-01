74d978daa5cb8bcfe941fae660e89e8a887447fc
SoumojjalSen <soumojjalsen@gmail.com>
2025-02-02 01:44:36 +0530

    feat: implement caching mechanism using Redis to store translations for improved performance

--- store_faq/settings.py
+++ store_faq/settings.py
@@ -54,6 +54,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Redis cache configuration
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 ROOT_URLCONF = 'store_faq.urls'
 
 TEMPLATES = [
